b584a4366919dd54c222458bc9eab32feed615d2
Mago Oscuro <Mago Oscuro@172.16.2.82>
2014-11-10 17:14:20 -0600

    cambio en archivo 1 ramam master

--- archivo 1.php
+++ archivo 1.php
@@ -1,8 +1,8 @@
-<?php
+<html>
+    <head>
+        <title>title</title>
+    </head>
+    <body>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    </body>
+</html>

13b3991f99f7ad2c87fa5964943fb95db66543a8
Oscuro <Oscuro@Dark>
2014-12-01 07:23:23 -0600

    Comienzo ,agregado de librerias y frameworks

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
+<?php
+include_once './conexion.php';
+
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <title>index</title>
-        
+       
     </head>
+      
     <body>
-        inicio del index
     </body>
 </html>

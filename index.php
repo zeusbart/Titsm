7b36e0840862237b379db63f61b184049fbde62e
GitWindow <bart@gitwindow>
2014-12-01 23:19:47 -0600

    Compostura de index, diseño y anexado de imagenes

--- index.php
+++ index.php
@@ -1,15 +1,52 @@
-<?php
-include_once './conexion.php';
-
-?>
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>index</title>
-       
-    </head>
-      
-    <body>
-    </body>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Title Page</title>
+
+		<!-- Bootstrap CSS -->
+                <link href="bootstrap_css/bootstrap.min.css" rel="stylesheet">
+                <link href="css/login.css" rel="stylesheet">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		 <div class="container">
+                <div class="row vertical-offset-100">
+                    <div class="col-md-4 col-md-offset-4">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <div class="row-fluid user-row">
+                                    <img src="img/logo_OSH.png " class="img-responsive" alt="Conxole Admin"/>
+                                </div>
+                            </div>
+                            <div class="panel-body">
+                                <form accept-charset="UTF-8" role="form" class="form-signin">
+                                    <fieldset>
+                                        <label class="panel-login">
+                                            <div class="login_result"></div>
+                                        </label>
+                                        <input class="form-control" placeholder="Username" id="username" type="text">
+                                        <input class="form-control" placeholder="Password" id="password" type="password">
+                                        <br></br>
+                                        <input class="btn btn-lg btn-success btn-block" type="submit" id="login" value="Login »">
+                                    </fieldset>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+		<!-- jQuery -->
+                <script src="js/jquery-1.10.2.js"></script>
+		<!-- Bootstrap JavaScript -->
+                <script src="bootstrap_js/bootstrap.min.js"></script>
+	</body>
 </html>
